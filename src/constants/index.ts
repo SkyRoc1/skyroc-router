978a2dada0ff750631956112e6f407327612ebb5
Ohh <1509326266@qq.com>
2025-06-26 23:17:33 +0800

    feat: add constants module

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+export const SKYROC_ROUTER_TYPES_MODULE_NAME = '@skyroc-router/types';
+
+export const REACT_ROUTER_MODULE_NAME = 'react-router/auto-routes';
+
+export const ROOT_ROUTE_NAME = 'Root';
+
+export const NOT_FOUND_ROUTE_NAME = 'NotFound';
+
+export const NO_FILE_INODE = -99;
+
+export const BUILT_IN_ROUTE = {
+  [ROOT_ROUTE_NAME]: '/',
+  [NOT_FOUND_ROUTE_NAME]: '*'
+} as const;
+
+export const CLI_CONFIG_SOURCE = 'skyroc-router.config';
+export const SHORT_CLI_CONFIG_SOURCE = 'sr.config';
