35b30505b43bd5614678de644186cb3cfb00c557
Ohh <1509326266@qq.com>
2025-06-26 22:55:29 +0800

    feat: add glob module

---
+++ src/core/glob.ts
@@ -0,0 +1,55 @@
+import path from 'node:path';
+import { stat } from 'node:fs/promises';
+import { normalizePath } from 'unplugin-utils';
+import { globSync } from 'tinyglobby';
+import { resolveImportPath } from '../shared';
+import type { ParsedAutoRouterOptions, ResolvedGlob } from '../types';
+
+export async function resolveGlobs(options: ParsedAutoRouterOptions) {
+  const { cwd, pageDir, pageInclude, pageExclude } = options;
+
+  const pageDirs = Array.isArray(pageDir) ? pageDir : [pageDir];
+
+  const pageGlobs = pageDirs.flatMap(dir => {
+    const $pageDir = path.resolve(cwd, dir);
+
+    const globs = globSync(pageInclude, {
+      cwd: $pageDir,
+      onlyFiles: true,
+      ignore: pageExclude
+    });
+
+    return globs.map(glob => resolveGlob(glob, dir, options));
+  });
+
+  const globs: ResolvedGlob[] = await Promise.all(
+    pageGlobs.map(async glob => {
+      const info = await stat(glob.filePath);
+
+      return {
+        ...glob,
+        inode: info.ino
+      };
+    })
+  );
+
+  return globs;
+}
+
+export function resolveGlob(glob: string, pageDir: string, options: Pick<ParsedAutoRouterOptions, 'cwd' | 'alias'>) {
+  const { cwd, alias } = options;
+
+  const $pageDir = path.resolve(cwd, pageDir);
+
+  const filePath = normalizePath(path.resolve($pageDir, glob));
+  const importPath = resolveImportPath(filePath, alias);
+
+  const resolvedGlob: Omit<ResolvedGlob, 'inode'> = {
+    pageDir,
+    glob,
+    filePath,
+    importPath
+  };
+
+  return resolvedGlob as ResolvedGlob;
+}
