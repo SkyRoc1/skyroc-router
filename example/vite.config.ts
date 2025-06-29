20ac2bbae01856683c0ecabbbca012d4f1704861
Ohh-889 <1509326266@qq.com>
2025-06-30 03:04:28 +0800

    feat: example vite config add alias

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import SkyrocRouter from 'skyroc-router';
+import { fileURLToPath } from 'node:url';
 
 
 
 // https://vite.dev/config/
 export default defineConfig({
-
-  
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   plugins: [react(), SkyrocRouter?.default({})],
 })
